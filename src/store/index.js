80f103dce5e5bf14187f60ae742ad81d4d0e3a60
rioto.nagano <kingdom950927@gmail.com>
2022-10-23 16:14:50 +0900

    Storeの作成

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import { createStore } from "redux";
 const initialState = {
   count: 1,
 };
-
+console.log(initialState)
 const reducer = (state = initialState) => {
   return state;
+
 };
 
 const store = createStore(reducer);
