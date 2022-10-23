309d6d1e5dc9803c52805cd0d3972f45f63010b9
rioto.nagano <kingdom950927@gmail.com>
2022-10-23 17:03:36 +0900

    Countコンポーネントの作成とmap関数を用いた繰り返し処理の実行

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import './App.css';
 import React from "react"
 import store from "./store/index"
 
+// 各コンポーネント
 import Greeting from './components/Greeting';
+import Count from './components/Count';
 
 function App() {
+  
   return (
     <>
       <Greeting />
       <h1>Redux Learn</h1>
       <p>Count:{store.getState().count}</p>
+      <Count />
     </>
   );
 }
---
+++ src/components/Count.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { useSelector } from "react-redux"
+
+
+const Count = () => {
+  const count = useSelector((state) => state.count)
+  const posts = useSelector((state) => state.posts)
+  return (
+    <>
+      <div>Countコンポーネント:{count}</div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Count
