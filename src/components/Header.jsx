d30e44e035159d4d87eef749cf51fa957b0bb44b
rioto.nagano <kingdom950927@gmail.com>
2022-10-26 06:15:25 +0900

    ヘッダーの位置を固定

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
 
 function Header() {
     return (
-        <div>
-            ヘッダー
-        </div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography>ヘッダー</Typography>
+          </Toolbar>
+        </AppBar>
     )
 }
 
