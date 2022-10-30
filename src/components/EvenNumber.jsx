1c6391ab38f97e35ce65163bbd460d1a51ceafc0
rioto.nagano <kingdom950927@gmail.com>
2022-10-30 11:26:36 +0900

    ページ更新時に起動する自動カウンターの作成

--- src/components/EvenNumber.jsx
+++ src/components/EvenNumber.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 const EvenNumber = (() => {
 
   const [birthday, setBirthday] = useState(0)
+  const [ count, setCount ] = useState(0);
 
   useEffect(() => {
     const countUp = (() => {
@@ -13,13 +14,27 @@ const EvenNumber = (() => {
     countUp();
   }, [birthday])
 
+  useEffect(() => {
+    const time = setInterval(() => {
+        setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(time);
+  }, []);
+
   return (
     <>
-     <div>今日は誕生日です。（この文章に意味はないです。）</div>
-     <p>{birthday}</p>
-     <div>
-       <button onClick={() => setBirthday(birthday + 1)}>更新ボタン</button>
-     </div>
+      <div>今日は誕生日です。（この文章に意味はないです。）</div>
+      <p>{birthday}</p>
+      <div>
+        <button onClick={() => setBirthday(birthday + 1)}>更新ボタン</button>
+      </div>
+      <br></br>
+      <br></br>
+      <div>
+        <p>カウントが始まりました。</p>
+        <p>{count}</p>
+      </div>
     </>
   )
 })
